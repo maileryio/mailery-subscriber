8e3db237682dd346f6248a4815bee4706d528cb9
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-15 09:39:25 +0300

    Development

--- src/Field/ImportStatus.php
+++ src/Field/ImportStatus.php
@@ -30,9 +30,13 @@ class ImportStatus
     /**
      * @param string $value
      */
-    public function __construct(
+    private function __construct(
         private string $value
-    ) {}
+    ) {
+        if (!in_array($value, self::getKeys())) {
+            throw new \InvalidArgumentException('Invalid passed value: ' . $value);
+        }
+    }
 
     /**
      * @return string
@@ -40,6 +44,20 @@ class ImportStatus
     public function __toString(): string
     {
         return $this->value;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getKeys(): array
+    {
+        return [
+            self::PENDING,
+            self::RUNNING,
+            self::PAUSED,
+            self::ERRORED,
+            self::COMPLETED,
+        ];
     }
 
     /**
